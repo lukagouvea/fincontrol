cbceff5a703eb3eaf99670d860d0b00873eeb8d7
luka.gouvea0 <luka.gouvea0@gmail.com>
2025-11-20 20:45:18 +0000

    fix routes

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,11 @@
 import api from './api';
 
 export const login = async (credentials: any) => {
-  const response = await api.post('/auth/login', credentials);
+  const response = await api.post('/users/login', credentials);
   return response.data;
 };
 
 export const signup = async (userInfo: any) => {
-  const response = await api.post('/auth/signup', userInfo);
+  const response = await api.post('/users/signup', userInfo);
   return response.data;
 };

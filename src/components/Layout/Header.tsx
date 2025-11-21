08fa6a900f08977e24a422a9efaf7aed212ffe20
Luka <luka.gouvea0@gmail.com>
2025-11-21 16:29:55 -0300

    remove button outside themetoggle

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -21,9 +21,9 @@ export const Header: React.FC = () => {
         </p>
       </div>
       <div className="flex items-center space-x-4">
-        <button className="p-2 text-gray-500 rounded-full hover:bg-gray-100">
+        <div className="p-2 text-gray-500 rounded-full hover:bg-gray-100">
           <ThemeToggle/>
-        </button>
+        </div>
         <div className="flex items-center space-x-2">
           <div className="w-10 h-10 bg-blue-100 text-blue-600 rounded-full flex items-center justify-center">
             <UserIcon className="w-5 h-5" />
